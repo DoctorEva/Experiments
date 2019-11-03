cef973c47ceb13e3a33e53c3a8766ce59f474d9e
Doctor Eva <dr.evarosalene1@gmail.com>
2019-11-03 16:32:32 -0600

    Added a new idea

--- Eva's Tetris/main.cpp
+++ Eva's Tetris/main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "tetris.h"
+#include "scores.h"
 
 int main(int argc, char** argv)
 {
@@ -11,5 +12,12 @@ int main(int argc, char** argv)
   getchar();
   int score = Tetris();
   printf("Final Score :: %d\nThanks for playing!\n", score);
+
+  char name[] = "DoctorEva";
+  scores_list s = scores_list("Scores.txt");
+  s.add_score( name, score);
+  s.dump_scores();
+  s.save_scores();
+
   return 0;
 }
